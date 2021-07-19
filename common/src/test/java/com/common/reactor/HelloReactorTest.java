a528c149649b3bfa42de9c453287e9c5293f6094
mvphjx <511572653@qq.com>
2021-07-19 19:47:59 +0800

    初识Reactor

---
+++ common/src/test/java/com/common/reactor/HelloReactorTest.java
@@ -0,0 +1,23 @@
+package com.common.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+/**
+ * 初识Reactor
+ * 反应式流规范定义了4种类型：Publisher、Subscriber、Subscription和Processor
+ * @author hjx
+ * @version 1.0
+ * @date 2021/7/19 19:40
+ */
+public class HelloReactorTest
+{
+    /**
+     * 创建Flux，并且添加一个订阅者Consumer
+     */
+    @Test
+    public void Hello(){
+        Flux<Integer> numberFlux = Flux.just(1, 2, 3, 4);
+        numberFlux.subscribe(f->System.out.println("Here's some number:"+f));
+    }
+}
